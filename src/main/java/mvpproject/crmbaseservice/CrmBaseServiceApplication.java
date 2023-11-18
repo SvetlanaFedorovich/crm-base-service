ef265309c2f556b01d5f4860be3cb05753e36609
fedor <fedorovich-lana@mail.ru>
2023-11-18 16:48:18 +0300

    MVP-1 init commit

---
+++ src/main/java/mvpproject/crmbaseservice/CrmBaseServiceApplication.java
@@ -0,0 +1,13 @@
+package mvpproject.crmbaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmBaseServiceApplication.class, args);
+    }
+
+}
