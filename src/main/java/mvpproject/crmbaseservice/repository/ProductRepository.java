bf17d97019355e5764e8e3be569eeaae9aae6d58
Admin <komikovsergey@gmail.com>
2023-11-28 20:28:36 +0300

    MVP-3 new annotation added and code corrected

--- src/main/java/mvpproject/crmbaseservice/repository/ProductRepository.java
+++ src/main/java/mvpproject/crmbaseservice/repository/ProductRepository.java
@@ -1,6 +1,6 @@
-package mvpproject.crmbaseservice.repositories;
+package mvpproject.crmbaseservice.repository;
 
-import mvpproject.crmbaseservice.entities.Product;
+import mvpproject.crmbaseservice.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
